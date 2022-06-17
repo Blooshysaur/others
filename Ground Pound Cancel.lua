82e348a53b2069b8850e7eae0e527f77fbad355c
Blooshysaur <75738280+Blooshysaur@users.noreply.github.com>
2022-06-17 21:29:32 +0200

    v1.0 - 17/06/2022

---
+++ Ground Pound Cancel.lua
@@ -0,0 +1,12 @@
+-- name: Ground Pound Cancel v1.0
+-- description: made by blooshy, allows Mario & co to cancel ground pound by pressing A while doing it.
+
+function gp_cancel(m)
+
+if m.action == ACT_GROUND_POUND  and (m.input & INPUT_A_PRESSED) ~= 0 then
+    set_mario_action(m, ACT_BUTT_SLIDE_AIR, 0)
+    m.vel.y = 0
+end
+end
+
+hook_event(HOOK_BEFORE_MARIO_UPDATE,gp_cancel)
